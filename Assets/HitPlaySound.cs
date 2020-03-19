233e055c013e483b4b057f4144cc9697b58d3972
kotaromurata <mtk130127@gmail.com>
2020-03-19 22:08:18 +0900

    LESSON7　課題
    ユニティちゃん以外と衝突時に効果音を鳴らす

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound: MonoBehaviour
+{
+    //当たった時の処理
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CubeTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (collision.gameObject.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
